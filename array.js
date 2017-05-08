ccd8efed360052b8c2573a3bbde23f7979a9c445
Rafael Augusto <rafaelvicio@icloud.com>
2017-05-08 12:48:30 -0300

    Source com Array

---
+++ array.js
@@ -0,0 +1,8 @@
+var Rx = require('@reactivex/rxjs');
+
+var numbers = [1,8,15,37];
+var source = Rx.Observable.from(numbers);
+
+var subscription = source.subscribe(
+  value => console.log(`My value is: ${value}`)
+)
