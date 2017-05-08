c1f85df88a72101a497f09bb6f325ddce51b3d4b
Rafael Augusto <rafaelvicio@icloud.com>
2017-05-08 12:33:53 -0300

    sobre subjects

--- subjectSub.js
+++ subjectSub.js
@@ -1,5 +1,22 @@
 var Rx = require('@reactivex/rxjs');
 
 var observable = Rx.Observable.create(observer => {
-
+  var i = 0;
+    interval = setInterval(() => {
+      i++;
+      observer.next(parseInt(Math.random()*10));
+      if(i > 5){
+        clearInterval(interval);
+        observer.complete();
+      }
+    }, 1000)
 })
+
+var subject = new Rx.Subject()
+var subSource = observable.subscribe(subject);
+
+var subscription = subject.subscribe(
+  value => console.log(`My value ${value}`),
+  err => console.log(`Error mesage: ${err}`),
+  () => console.log('Complete!')
+)
