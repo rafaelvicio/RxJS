fc8e641ea59ef15e13ed8daf83a339d381f63850
Rafael Augusto <rafaelvicio@icloud.com>
2017-05-09 14:23:36 -0300

    List e Subject

---
+++ replaySubject.js
@@ -0,0 +1,17 @@
+var Rx = require('@reactivex/rxjs');
+
+var subject = new Rx.ReplaySubject(2);
+var subscription = subject.subscribe(
+    value => console.log(`My Value is ${value}`),
+    err => console.log(`Error message: ${err}`),
+    () => console.log('Completed!')
+)
+
+var counter = 5,
+    interval = setInterval(()=>{
+        subject.next(counter--);
+        if(!counter){
+            clearInterval(interval);
+            subject.complete()
+        }
+    }, 1000);
